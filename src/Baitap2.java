a1399f7abef742ea078802d0c6bdaa66d4d8b7ed
Huong14 <thienhuongbluesky14@gmail.com>
2026-06-07 18:26:09 +0900

    upload

---
+++ src/Baitap2.java
@@ -0,0 +1,10 @@
+public class Baitap2 {
+    public static void main(String[] args) {
+        String name = "Nguyễn Văn A";
+        int age = 22;
+        String address = "Cà Mau";
+
+        System.out.printf("Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s.%n",
+                name, age, address);
+    }
+}
